10da90dba5b04668608fceb9c4a57a4af3f9a30f
user <vikakovai19@yahoo.com>
2021-10-03 21:13:36 -0400

    added void

--- src/First.java
+++ src/First.java
@@ -1,9 +1,14 @@
 public class First {
     public static void main(String[] args) {
+
         //added main
+    }
+    public void str(){
+
     }
     //we created new file
    // First Git Practice
     //added
     //added second brabch
+
 }
