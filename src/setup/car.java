ce62c11f820d3c784d923c11041ee52c409bc5d4
user <vikakovai19@yahoo.com>
2021-10-03 23:06:40 -0400

    made car in master branch

--- src/setup/car.java
+++ src/setup/car.java
@@ -1,16 +1,17 @@
 package setup;
 
 public class car {
-<<<<<<< HEAD
+
     //comments, this is master branch
+
     //will have conflict
-=======
+
     //this our car
 
     //more text
 
     //more
 
->>>>>>> james
+
 
 }
